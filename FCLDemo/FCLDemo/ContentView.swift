bd724148055739c8fe8a02d8f9f1c991bb1a82a5
chris <87675168+chris-zedio@users.noreply.github.com>
2021-07-30 11:18:57 +0800

    Add Swifter

--- FCLDemo/FCLDemo/ContentView.swift
+++ FCLDemo/FCLDemo/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by lmcmz on 28/7/21.
 //
 
+import Swifter
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,21 @@ struct ContentView: View {
 //        "https://port.onflow.org"
 //        "https://fcl-demo.netlify.app"
         Webview(url: URL(string: "http://127.0.0.1:3000")!)
+    }
+
+    lazy var server: HttpServer = {
+        let server = HttpServer()
+        server["/fcl/authn"] = { request in
+            print(request.method)
+            print(request.headers)
+            print(request.params)
+            return HttpResponse.ok(.json([]))
+        }
+        return server
+    }()
+
+    init() {
+        try? server.start()
     }
 }
 
